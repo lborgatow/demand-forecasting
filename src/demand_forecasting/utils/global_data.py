70571f4bf78828977ea839d5ee46c492b18bb7b3
lborgatow <lborgatow@gmail.com>
2024-04-20 09:20:36 -0300

    Criando classes das métricas (Metrics) e dos dados globais (GlobalData) do projeto

---
+++ src/demand_forecasting/utils/global_data.py
@@ -0,0 +1,51 @@
+from typing import List, Union
+from dataclasses import dataclass
+
+import polars as pl
+import numpy as np
+
+from .metrics import Metrics
+
+
+@dataclass
+class GlobalData(Metrics):
+    """Class to define the global data that will be used in all executions."""
+
+    data: pl.DataFrame
+    metrics: List[str]
+
+
+    def filter_and_sort_data(self, unique_id: str) -> pl.DataFrame:
+        """Filter the DataFrame data according to the unique id and 
+        sort data accordind to the date.
+
+        Arguments:
+            unique_id (str): store-item unique id.
+        
+        Returns:
+            pl.DataFrame: DataFrame with the filtered data.
+        """
+
+        return self.data.filter(pl.col("unique_id") == unique_id).sort(by="date")
+
+
+    def get_metrics(self, y_true: Union[List[float], np.ndarray, pl.Series], 
+                    y_pred: Union[List[float], np.ndarray, pl.Series],
+                    y_naive: Union[List[float], np.ndarray, pl.Series]) -> List[float]:
+        """Get the metrics when executing the "run_metrics" method of the "Metrics" class.
+
+        Arguments:
+            y_true (Union[List[float], np.ndarray, pl.Series]): True values.
+            y_pred (Union[List[float], np.ndarray, pl.Series]): Predicted values.
+            y_naive (Union[List[float], np.ndarray, pl.Series]): Values predicted by the naive model.
+
+        Returns:
+            List[float]: List with metric values.
+        """
+
+        return self.run_metrics(
+            metrics=self.metrics, 
+            y_true=y_true, 
+            y_pred=y_pred, 
+            y_naive=y_naive
+        )
---
+++ src/demand_forecasting/utils/metrics.py
@@ -0,0 +1,94 @@
+from typing import List, Union
+from dataclasses import dataclass
+
+import polars as pl
+import numpy as np
+
+
+@dataclass
+class Metrics:
+    """Class to define and obtain performance metrics."""
+
+
+    def smape(self, y_true: Union[List[float], np.ndarray, pl.Series], 
+              y_pred: Union[List[float], np.ndarray, pl.Series]) -> float:
+        """Calculates the Symmetric Mean Absolute Percentage Error (sMAPE) for a forecast.
+
+        Args:
+            y_true (Union[List[float], np.ndarray, pl.Series]): True values.
+            y_pred (Union[List[float], np.ndarray, pl.Series]): Predicted values.
+
+        Returns:
+            float: sMAPE value.
+        """
+        
+        y_true = np.array(y_true).ravel()
+        y_pred = np.array(y_pred).ravel()
+        
+        metric = 2 * np.abs(y_pred - y_true) / (np.abs(y_pred) + np.abs(y_true))
+        aux = np.where((y_true == 0) & (y_pred == 0), 0.0, metric)
+
+        return np.mean(aux)
+
+
+    def rmsle(self, y_true: Union[List[float], np.ndarray, pl.Series], 
+             y_pred: Union[List[float], np.ndarray, pl.Series]) -> float:
+        """Calculates the Root Mean Squared Log Error (RMSLE) for a forecast.
+
+        Args:
+            y_true (Union[List[float], np.ndarray, pl.Series]): True values.
+            y_pred (Union[List[float], np.ndarray, pl.Series]): Predicted values.
+
+        Returns:
+            float: RMSLE value.
+        """
+        
+        y_true = np.array(y_true).ravel()
+        y_pred = np.array(y_pred).ravel()
+
+        return np.sqrt(np.mean((np.log1p(y_true) - np.log1p(y_pred)) ** 2))
+    
+    
+    def owa(self, y_true: Union[List[float], np.ndarray, pl.Series], 
+            y_pred: Union[List[float], np.ndarray, pl.Series],
+            y_naive: Union[List[float], np.ndarray, pl.Series]) -> float:
+        """Calculates the Overall Weighted Average (RMSLE) for a forecast.
+
+        Args:
+            y_true (Union[List[float], np.ndarray, pl.Series]): True values.
+            y_pred (Union[List[float], np.ndarray, pl.Series]): Predicted values.
+            y_naive (Union[List[float], np.ndarray, pl.Series]): Values predicted by the naive model.
+
+        Returns:
+            float: OWA value.
+        """
+        
+        model_smape = self.smape(y_true, y_pred)
+        naive_smape = self.smape(y_true, y_naive)
+        
+        model_rmsle = self.rmsle(y_true, y_pred)
+        naive_rmsle = self.rmsle(y_true, y_naive)
+        
+        return np.mean((model_smape / naive_smape) + (model_rmsle / naive_rmsle))
+
+
+    def run_metrics(self, metrics: List[str], 
+                    y_true: Union[List[float], np.ndarray, pl.Series], 
+                    y_pred: Union[List[float], np.ndarray, pl.Series], 
+                    y_naive: Union[List[float], np.ndarray, pl.Series]) -> List[float]:
+        """Get performance metrics results.
+
+        Arguments:
+            metrics (List[str]): List of metrics being calculated.
+            y_true (Union[List[float], np.ndarray, pl.Series]): True values.
+            y_pred (Union[List[float], np.ndarray, pl.Series]): Predicted values.
+            y_naive (Union[List[float], np.ndarray, pl.Series]): Values predicted by the naive model.
+
+        Returns:
+            List[float]: List of metrics in order.
+        """
+
+        return [
+            self.__getattribute__(metric)(y_true, y_pred) if metric != "owa" 
+            else self.__getattribute__(metric)(y_true, y_pred, y_naive) for metric in metrics
+        ]
