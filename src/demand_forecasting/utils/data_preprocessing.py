8606fb76e76b0bb797da46c00b523a4e68d9730a
lborgatow <lborgatow@gmail.com>
2024-04-20 17:07:21 -0300

    Criando função para formatar os nomes das colunas e formatação dos registros (dígitos) das variáveis store e item

--- src/demand_forecasting/utils/data_preprocessing.py
+++ src/demand_forecasting/utils/data_preprocessing.py
@@ -3,12 +3,31 @@ from typing import List
 import polars as pl
 
 
+def format_single_digit(df: pl.DataFrame) -> pl.DataFrame:
+    """Format the records of the variables "store" and "item" 
+    that have only one digit.
+
+    Args:
+        df (pl.DataFrame): DataFrame with the temporal data 
+        of the store and items.
+
+    Returns:
+        pl.DataFrame: DataFrame with formatted variables.
+    """
+    
+    return df.with_columns(
+        store=pl.col("store").cast(str).str.zfill(2),
+        item=pl.col("item").cast(str).str.zfill(2)
+    )
+
+
 def create_unique_id(df: pl.DataFrame) -> pl.DataFrame:
     """Create a unique identifier for the items to forecast. 
     This function combines two codes: the store code and the item code.
 
     Args:
-        df (pl.DataFrame): DataFrame with the temporal data of the items.
+        df (pl.DataFrame): DataFrame with the temporal data of the stores 
+        and items.
 
     Returns:
         pl.DataFrame: Dataframe with the unique identifier included.
@@ -17,6 +36,22 @@ def create_unique_id(df: pl.DataFrame) -> pl.DataFrame:
     return df.with_columns(
         pl.concat_str(["store", "item"], separator="_").alias("uid")
     )
+
+
+def format_column_names(df: pl.DataFrame) -> pl.DataFrame:
+    """Format column names.
+
+    Args:
+        df (pl.DataFrame): DataFrame with temporal data.
+
+    Returns:
+        pl.DataFrame: DataFrame with column names formatted.
+     """
+    
+    return df.with_columns(
+        pl.col("date").str.to_datetime().alias("ds"),
+        pl.col("sales").alias("y")
+    ).drop(["date", "sales"])
 
 
 def process_data(data: pl.DataFrame) -> pl.DataFrame:
@@ -29,11 +64,9 @@ def process_data(data: pl.DataFrame) -> pl.DataFrame:
         pl.DataFrame: DataFrame processed.
     """
 
-    df = create_unique_id(data)
-    df = df.with_columns(
-        pl.col("date").str.to_datetime().alias("ds"),
-        pl.col("sales").alias("y")
-    ).drop(["date", "sales"])
+    df = format_single_digit(data)
+    df = create_unique_id(df)
+    df = format_column_names(df)
 
     return df.select(["ds", "uid", "store", "item", "y"])
 
